32814f0ac300f0a026fc5628645eda9a4ca660b1
Alejandro Lopez <alejandro@joymo.tv>
2024-02-26 10:03:37 +0100

    chore: simplify lib tsconfig

--- projects/common/src/lib/common.component.ts
+++ projects/common/src/lib/common.component.ts
@@ -3,10 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'lib-common',
   standalone: true,
-  imports: [],
   template: `<p>I'm a (reusable) CommonComponent!</p>`,
-  styles: ``
 })
-export class CommonComponent {
-
-}
+export class CommonComponent { }
