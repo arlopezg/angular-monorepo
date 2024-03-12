f43133f1d3fba7587568052375014d09e8c1eacd
Alejandro Lopez <alejandro@joymo.tv>
2024-03-12 10:25:07 +0100

    chore: display current platform

--- projects/project-ssr/src/app/app.component.ts
+++ projects/project-ssr/src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component, Inject, LOCALE_ID, afterRender } from '@angular/core';
+import { Component, Inject, LOCALE_ID, PLATFORM_ID, afterRender } from '@angular/core';
 import { CommonComponent } from '@common-pkg';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonComponent],
-  template: `<lib-common [greeting]="greeting" clientName="SSR App"/>`,
+  template: `<lib-common [greeting]="greeting" [clientName]="appName"/>`,
 })
 export class AppComponent {
+  readonly appName: string;
   readonly greeting = $localize`:@@welcomeTo:Welcome to`;
 
-  constructor(@Inject(LOCALE_ID) private locale: string) {
+  constructor(
+    @Inject(LOCALE_ID) private locale: string,
+    @Inject(PLATFORM_ID) public platformId: string,
+  ) {
+    this.appName = [this.platformId, "app"].join(" ");
+
     console.log("Locale:", this.locale);
 
     afterRender(() => {
